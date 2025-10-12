3b6743889212a0a04483669982974ce238f5dec4
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2025-10-12 18:42:11 +0500

    fix: made product variant id auto generated

--- src/main/java/com/igriss/ListIn/publication/entity/ProductVariant.java
+++ src/main/java/com/igriss/ListIn/publication/entity/ProductVariant.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -33,6 +35,7 @@ public class ProductVariant {
 
     @Id
     @Column(name = "variant_id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID variantId;
 
     @Column(name = "size")
