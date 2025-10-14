f4a7056fbfda37d8949c035161d64716e8ead319
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2025-10-14 11:27:47 +0500

    fix: bug fix on productVarient.images lazy loading [made images eagerly loading using query]

--- src/main/java/com/igriss/ListIn/publication/repository/ProductVariantRepository.java
+++ src/main/java/com/igriss/ListIn/publication/repository/ProductVariantRepository.java
@@ -2,11 +2,17 @@ package com.igriss.ListIn.publication.repository;
 
 import com.igriss.ListIn.publication.entity.ProductVariant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ProductVariantRepository extends JpaRepository<ProductVariant, UUID> {
 
+    @Query("""
+    select distinct pv from ProductVariant pv
+    left join fetch pv.images
+    where pv.publication.id = :publicationId
+    """)
     List<ProductVariant> findByPublication_Id(UUID publicationId);
 }
